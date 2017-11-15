e6ebe96df10f51f5ab5dc3e2a601684ad9bd702d
vkhemlani96 <vkhemlani96@gmail.com>
2017-11-15 15:40:17 -0500

    Added Bluetooth Driver

--- app/src/main/java/com/ubru/brurista/fragments/BrewingFragment.java
+++ app/src/main/java/com/ubru/brurista/fragments/BrewingFragment.java
@@ -6,20 +6,43 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.ubru.brurista.BluetoothDriver;
 import com.ubru.brurista.R;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class BrewingFragment extends UserFragment {
+
+    private Timer timer;
+    private String message = "test1";
+    private TimerTask timerTask = new TimerTask() {
+        @Override
+        public void run() {
+            BluetoothDriver.sendMessage(message);
+            if (message.equals("test1")) {
+                message = "test2";
+            } else {
+                message = "test1";
+            }
+        }
+    };
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_brewing, container, false);
+
         rootView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (timer != null) {
+                    timer.cancel();
+                }
                 getActivity().finish();
             }
         });
@@ -27,4 +50,11 @@ public class BrewingFragment extends UserFragment {
         return rootView;
     }
 
+    @Override
+    public void onSlideTo() {
+        BluetoothDriver.init(getActivity());
+
+        timer = new Timer();
+        timer.scheduleAtFixedRate(timerTask, 0, 500);
+    }
 }
