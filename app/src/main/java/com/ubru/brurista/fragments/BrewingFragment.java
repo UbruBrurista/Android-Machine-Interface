d708f663073cdcb731ea878b08922b5cd693ac23
vkhemlani96 <vkhemlani96@gmail.com>
2018-04-12 15:04:37 -0400

    Added stages to brewing

--- app/src/main/java/com/ubru/brurista/fragments/BrewingFragment.java
+++ app/src/main/java/com/ubru/brurista/fragments/BrewingFragment.java
@@ -1,29 +1,44 @@
 package com.ubru.brurista.fragments;
 
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 import com.google.android.things.pio.Gpio;
 import com.google.android.things.pio.GpioCallback;
 import com.ubru.brurista.GPIODriver;
 import com.ubru.brurista.GPIODriver.HandlerCallback;
 import com.ubru.brurista.R;
 import com.ubru.brurista.UARTDriver;
+import com.ubru.brurista.UserActivity;
 
 import java.util.Arrays;
 
 public class BrewingFragment extends UserFragment {
+
+    private String[] STAGES = {
+            "Preheating",
+            "Grinding",
+            "Brewing",
+            "Cleaning Up"
+    }
+    private int stageIndex = 0;
+    private TextView stageView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_brewing, container, false);
 
-
+        stageView = rootView.findViewById(R.id.brewing_stage);
 
         rootView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,8 +67,23 @@ public class BrewingFragment extends UserFragment {
         GPIODriver.write(bytesToSend, new GpioCallback() {
             @Override
             public boolean onGpioEdge(Gpio gpio) {
-                System.out.println("CALLBACK------");
-                BrewingFragment.this.getActivity().finish();
+
+                stageIndex++;
+                if (stageIndex >= STAGES.length) {
+                    new AlertDialog.Builder(getContext())
+                            .setMessage("Enjoy your drink!")
+                            .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    BrewingFragment.this.getActivity().finish();
+                                }
+                            })
+                            .show();
+                } else {
+                    stageView.setText(STAGES[stageIndex] + "...");
+                }
+
+
                 return false;
             }
         });
